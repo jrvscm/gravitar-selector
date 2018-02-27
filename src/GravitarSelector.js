174a2d4c5dbba04fb631bae3af6b7ded434bb846
Chris Jarvis <jrvscm@gmail.com>
2018-02-27 11:35:03 -0800

    first commit

---
+++ src/GravitarSelector.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './GravitarSelector.css';
+
+class GravitarSelector extends Component {
+  render() {
+    return (
+      <div className="GravitarSelector">
+
+      </div>
+    );
+  }
+}
+
+export default GravitarSelector;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GravitarSelector from './GravitarSelector';
+
+ReactDOM.render(<GravitarSelector />, document.getElementById('root'));
+
